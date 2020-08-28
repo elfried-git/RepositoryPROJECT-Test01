88674746f41b624d2ad2830b6d043323057eaed5
elfried-git <70368186+elfried-git@users.noreply.github.com>
2020-08-28 10:44:59 +0200

    Add files via upload
    All files for the First Projet test in Katalon 
    
    Thank you !

---
+++ BANQUES.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>BANQUES</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>daeb0cbf-36b5-49d6-a4ae-80c2d0568d79</testSuiteGuid>
+   <testCaseLink>
+      <guid>306c7493-c3b0-42e2-b09d-ceb514c586a8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>05fa8daf-5ed3-4fb1-909c-4aa7f9e552e0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case02</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
